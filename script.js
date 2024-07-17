3ea010936e4d0df5abbe225b3129a5d3d5346141
“arthur” <arthurcesar106@gmail.com>
2024-07-17 18:24:27 -0300

    alterações

--- script.js
+++ script.js
@@ -40,3 +40,15 @@ function togglemenu(){
     menumobile.className = "menu-mobile-a"
   }
 }
+
+const audio = document.getElementById('music');
+    const playButton = document.getElementById('play-button');
+    const pauseButton = document.getElementById('pause-button');
+
+    playButton.addEventListener('click', () => {
+        audio.play();
+    });
+
+    pauseButton.addEventListener('click', () => {
+        audio.pause();
+    });
