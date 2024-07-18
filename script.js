6a6eef466b5903bd7687da7693f64a2a3b505a0f
“arthur” <arthurcesar106@gmail.com>
2024-07-17 21:28:24 -0300

    menu mobile

--- script.js
+++ script.js
@@ -40,15 +40,3 @@ function togglemenu(){
     menumobile.className = "menu-mobile-a"
   }
 }
-
-const audio = document.getElementById('music');
-    const playButton = document.getElementById('play-button');
-    const pauseButton = document.getElementById('pause-button');
-
-    playButton.addEventListener('click', () => {
-        audio.play();
-    });
-
-    pauseButton.addEventListener('click', () => {
-        audio.pause();
-    });
